c8765540be72fddd8ddbc8addb5648898083a748
Innocent97 <innocentkyalo@gmail.com>
2021-07-06 22:54:43 +0300

    First Commit of the GitHub API Test android app.

---
+++ app/src/main/java/kyalo/innocent/githubapitest/models/github_users/License.kt
@@ -0,0 +1,9 @@
+package kyalo.innocent.githubapitest.models.github_users
+
+data class License(
+    val key: String?,
+    val name: String?,
+    val node_id: String?,
+    val spdx_id: String?,
+    val url: String?
+)
--- app/src/main/java/kyalo/innocent/githubapitest/ui/github_user_profile/GitHubUserProfileFragment.kt
+++ app/src/main/java/kyalo/innocent/githubapitest/ui/github_user_profile/GitHubUserProfileFragment.kt
@@ -1,21 +1,17 @@
 package kyalo.innocent.githubapitest.ui.github_user_profile
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
 import es.dmoral.toasty.Toasty
 import kyalo.innocent.githubapitest.R
 import kyalo.innocent.githubapitest.databinding.FragmentGithubUserProfileBinding
-import kyalo.innocent.githubapitest.models.github_users.GitHubUserModel
 
 class GitHubUserProfileFragment: Fragment() {
 
@@ -47,20 +43,9 @@ class GitHubUserProfileFragment: Fragment() {
 
         userProfileViewModel.username.value?.let { userProfileViewModel.getUserData(it) }
 
-       /* userProfileViewModel.username.value?.let { userProfileViewModel.searchUserInDB(it) }
-
-        Toasty.info(requireContext(), "${userProfileViewModel.username.value}", Toasty.LENGTH_SHORT).show()
-
-        // Observe searched user property
-        userProfileViewModel.gitHubUser?.observe(viewLifecycleOwner,) { remoteUser ->
-            userProfileBinding.gitHubUser = remoteUser
-        }*/
-
         userProfileViewModel.username.value?.let {
             userProfileViewModel.searchUserInDB(it)?.observe(viewLifecycleOwner, { user ->
                 userProfileBinding.gitHubUser = user
-                if (user != null)
-                    Toasty.info(requireContext(), "${user.login} ${user.email}", Toasty.LENGTH_LONG).show()
             })
         }
 
